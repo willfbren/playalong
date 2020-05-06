eb0b5400528c504b72f7ca31aad625e306b2bfeb
RafaS92 <manrvaldez..92@gmail.com>
2020-05-06 09:41:43 -0500

    test

--- frontend/authorization_code/public/index.js
+++ frontend/authorization_code/public/index.js
@@ -1,19 +1,20 @@
-const API_DOMAIN = 'http://localhost:3000/cable'
-let api = WarpCable(API_DOMAIN)
+const API_DOMAIN = "http://localhost:3000/cable";
+let api = WarpCable(API_DOMAIN);
 
 function getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
+  var hashParams = {};
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  while ((e = r.exec(q))) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
   }
-
-let params = getHashParams()
-if (params.access_token == undefined) {
-    window.location.replace('/login')
+  return hashParams;
 }
-let spotify_api = new SpotifyWebApi
-spotify_api.setAccessToken(params.access_token)
+
+let params = getHashParams();
+if (params.access_token == undefined) {
+  window.location.replace("/login");
+}
+let spotify_api = new SpotifyWebApi();
+spotify_api.setAccessToken(params.access_token);
